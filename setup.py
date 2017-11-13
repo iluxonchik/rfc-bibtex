12725ece92f75faf4e38461b2a7c3a73aa6cc632
ILUXONCHIK <iluxon4ik@hotmail.com>
2017-11-13 00:43:15 +0000

    chore: update version in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   author = 'Illya Gerasymchuk',
   author_email = 'giluxonchik@gmail.com',
   url = 'https://github.com/iluxonchik/rfc-bibtex/',
-  download_url = 'https://github.com/iluxonchik/rfc-bibtex/archive/0.1.tar.gz',
+  download_url = 'https://github.com/iluxonchik/rfc-bibtex/archive/0.2.tar.gz',
   keywords = ['rfc', 'internet draft', 'latex', 'bibtex', 'ietf'],
   scripts=['bin/rfc-bibtex', 'bin/rfc_bibtex', 'bin/rfcbibtex'],
   classifiers = [],
