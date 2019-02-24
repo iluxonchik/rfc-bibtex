e4958887820a0c625bcad9bacca3ba48a83bce41
Illya Gerasymchuk <illya@iluxonchik.me>
2019-02-24 13:05:10 +0000

    refactor integraiton tests
    Basic integration tests are now ready. This closes #15

---
+++ rfc_bibtex/tests/integration/base.py
@@ -0,0 +1,7 @@
+"""
+Base test class for all integraiton tests classes.
+"""
+import unittest
+
+class BaseRFCBibTexIntegrationTestCase(unittest.TestCase):
+    TEST_RESOURCES_PATH = 'rfc_bibtex/tests/integration/resources/'
--- rfc_bibtex/tests/integration/test_obtained_bibtex_from_raw_input.py
+++ rfc_bibtex/tests/integration/test_obtained_bibtex_from_raw_input.py
@@ -3,7 +3,7 @@ Integration tests related to reading raw list of RFCs providied in a text file (
 and directly as command-line args.
 """
 import vcr
-import unittest
+from .base import BaseRFCBibTexIntegrationTestCase
 from rfc_bibtex.rfc_bibtex import RFCBibtex
 
 """
@@ -16,14 +16,16 @@ NOTE: the tests are written with the migrations that will be done in considerati
       tests were adapted to work in both of them. This is why some assertions might
       seem like incomplete. Later, the tests should and will be refactored.
 """
-class TestObtainedBibtexFromRawRFCInput(unittest.TestCase):
+class TestObtainedBibtexFromRawRFCInput(BaseRFCBibTexIntegrationTestCase):
+    TLS_RFCS_FILE_PATH = BaseRFCBibTexIntegrationTestCase.TEST_RESOURCES_PATH + "tls_rfcs.txt"
+
     def setUp(self):
         pass
 
     def tearDown(self):
         pass
 
-    @vcr.use_cassette(path="rfc_bibtex/tests/integration/resources/fixtures/vcr_cassettes/synopsis.yaml")
+    @vcr.use_cassette(path='rfc_bibtex/tests/integration/resources/fixtures/vcr_cassettes/synopsis.yaml')
     def test_reading_rfcs_from_command_line_returns_expected_latex(self):
         rfc_bibtex = RFCBibtex(['RFC5246', 'draft-ietf-tls-tls13-21', 'RFC8446'])
         entries = list(rfc_bibtex.bibtex_entries)
@@ -40,9 +42,9 @@ class TestObtainedBibtexFromRawRFCInput(unittest.TestCase):
         self.assertIn("2018", entries[2])
         self.assertIn("RFC Editor", entries[2])
 
-    @vcr.use_cassette(path="rfc_bibtex/tests/integration/resources/fixtures/vcr_cassettes/synopsis.yaml")
+    @vcr.use_cassette(path='rfc_bibtex/tests/integration/resources/fixtures/vcr_cassettes/synopsis.yaml')
     def test_reading_rfcs_from_file_returns_expected_latex(self):
-        rfc_bibtex = RFCBibtex(in_file_name='rfc_bibtex/tests/integration/resources/tls_rfcs.txt')
+        rfc_bibtex = RFCBibtex(in_file_name=self.TLS_RFCS_FILE_PATH)
         entries = list(rfc_bibtex.bibtex_entries)
         self.assertIn("RFC5246", entries[0])
         self.assertIn("The Transport Layer Security (TLS) Protocol Version 1.2", entries[0])
